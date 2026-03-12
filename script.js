9091a00b25e808bf47f778af119c39d87ef58f5f
raghul7108 <raghulraja43@gmail.com>
2026-03-12 21:19:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+function submitFeedback(){
+
+var name=document.getElementById("name").value;
+var regno=document.getElementById("regno").value;
+var department=document.getElementById("department").value;
+var subject=document.getElementById("subject").value;
+var teacher=document.getElementById("teacher").value;
+var rating=document.getElementById("rating").value;
+var feedback=document.getElementById("feedback").value;
+
+var url="https://script.google.com/macros/s/AKfycbxV4s5XFkj40Gci7BsTlmUBtrdursJIwDqiIEbIPTp0LVCUNG44CZLZNwLF89BtzllE/exec";
+
+url += "?name="+name+
+"&regno="+regno+
+"&department="+department+
+"&subject="+subject+
+"&teacher="+teacher+
+"&rating="+rating+
+"&feedback="+feedback;
+
+fetch(url)
+.then(res=>res.text())
+.then(data=>{
+document.getElementById("popup").style.display="flex";
+});
+
+}
+
+function closePopup(){
+
+document.getElementById("popup").style.display="none";
+
+}
